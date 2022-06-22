37a0140a1278e0feae4e195a6948b32b18974ed6
ed-deloboy <edone.olimp@yandex.ru>
2022-06-22 17:55:13 +0300

    Fix

--- pages/about.php
+++ pages/about.php
@@ -1,24 +1,42 @@
-<section class="about-page row">
+<div class="about-page row">
     <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="assets/images/gallery/10.png" alt="..." class="card-img">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text"><small class="">Last updated 3 mins ago</small>
-                                </p>
-                            </div>
-                        </div>
+        <section class="row flex-lg-row-reverse align-items-center g-5 py-5 pb-3">
+            <h2 class="pb-2 ">Почему мы</h2>
+            <div class="col-10 col-sm-8 col-lg-6">
+                <img src="https://bootstrap-4.ru/docs/5.2/examples/heroes/bootstrap-themes.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+            </div>
+            <div class="col-lg-6">
+                <h1 class="display-5 fw-bold lh-1 mb-3">Responsive left-aligned hero with image</h1>
+                <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                </div>
+            </div>
+        </section>
+
+        <section class="row ">
+            <div class="pt-5 pb-5">
+                <h1 class="display-5 fw-bold">Обучающие вебинары</h1>
+                <p class="col-md-8 fs-4">Мы отсняли и продолжаем снимать большое количество обучающих вебинаров благодаря которым вы будете всегда в тренде</p>
+                <button class="btn btn-primary btn-lg" type="button">Смотреть вебинары</button>
+            </div>
+        </section>
+        <section class="mb-5">
+            <div class="row align-items-md-stretch">
+                <div class="col-md-6">
+                    <div class="h-100 p-5 text-white bg-dark rounded-3">
+                        <h2>Change the background</h2>
+                        <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then, mix and match with additional component themes and more.</p>
+                        <button class="btn btn-outline-light" type="button">Example button</button>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="h-100 p-5 bg-light border rounded-3">
+                        <h2>Add borders</h2>
+                        <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of both column's content for equal-height.</p>
+                        <button class="btn btn-outline-secondary" type="button">Example button</button>
                     </div>
                 </div>
             </div>
-
-        </div>
+        </section>
     </div>
-</section>
+</div>
